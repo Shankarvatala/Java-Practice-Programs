c6a6d6fb10a0dc25631f11237e353fd7771b2a45
Shankarvatala <sankarvatala@gmail.com>
2024-10-08 10:19:56 +0530

    Adding two methods and Data type relsted progrsmd

---
+++ src/practise_programs/AddingTwoMethods.java
@@ -0,0 +1,20 @@
+package practise_programs;
+
+public class AddingTwoMethods {
+	void a(int a) {
+		System.out.println();
+	}
+    void b(int b) {
+    	System.out.println(  );
+    	
+    }
+    
+	public static void main(String[] args) {
+		
+		AddingTwoMethods obj=new AddingTwoMethods();
+		obj.a(7);
+		obj.b(8);
+		
+	}
+
+}
---
+++ src/practise_programs/DataType_.java
@@ -0,0 +1,15 @@
+package practise_programs;
+
+public class DataType_ {
+
+	public static void main(String[] args) {
+		int num=10;
+		int number=20;
+		int sum;
+		sum=num+number;
+		System.out.println(sum);
+		
+
+	}
+
+}
