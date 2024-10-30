d15d00945aa33eb87c4d5c61f458281decd5cc5d
Shankarvatala <sankarvatala@gmail.com>
2024-10-30 14:18:21 +0530

    data type

---
+++ src/datatypes/Int_Demo_Program.java
@@ -0,0 +1,14 @@
+package datatypes;
+
+public class Int_Demo_Program {
+
+	public static void main(String[] args) {
+		//addition *********************************
+		int num=100;
+		int num2=900;
+		int sum;
+		sum=num+num2;
+		System.out.println(sum);
+	}
+
+}
